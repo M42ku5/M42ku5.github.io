67f51113c23bb99a4514b335c349e1280b651c45
Markus <51908256+M42ku5@users.noreply.github.com>
2021-04-10 22:18:48 +0200

    improves styling and adds copy function

--- jokes/scripts.js
+++ jokes/scripts.js
@@ -1,10 +1,30 @@
-function loadJoke() {
+const loadJoke = () => {
   fetch("https://official-joke-api.appspot.com/random_joke")
     .then((response) => response.json())
     .then((data) => {
+      const part1 = data.setup;
+      const part2 = data.punchline;
+      const output1 = part1 + "<br>" + part2;
+      const output2 = part1 + "\n" + part2;
       const elem = document.createElement("h1");
-      elem.innerHTML = data.setup + " " + data.punchline;
+      elem.innerHTML = output1;
       document.body.prepend(elem);
+      console.log(output2);
+
+      const copyToClipboard = str => {
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        console.log('copied: '+output2);
+      };
+      copyToClipboard(output2);
+
     });
 }
 document.addEventListener("DOMContentLoaded", function (event) {
