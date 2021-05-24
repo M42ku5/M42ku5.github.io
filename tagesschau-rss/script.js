94e3d5833c30e04bba5230331a7c6bd06a041ad9
Markus <51908256+M42ku5@users.noreply.github.com>
2021-05-24 15:17:48 +0200

    adds news description

--- tagesschau-rss/script.js
+++ tagesschau-rss/script.js
@@ -5,14 +5,15 @@ let parser = new DOMParser();
 let xmlDoc = parser.parseFromString(y,"text/xml");
 let txt = '';
 [...xmlDoc.querySelectorAll('item')].slice(0, 10).forEach(e=>{
-txt += '<p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('title').innerHTML + '</a></p>';
+txt += '<details><summary>' + e.querySelector('title').innerHTML + '</summary><p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('description').innerHTML + '</a></p></details>';
 });
 document.querySelector('main').innerHTML = txt + '<p class="more">mehr anzeigen</p>';
-document.querySelector('.more').addEventListener('click',()=>{
-document.querySelectorAll('p').forEach(e=>e.remove());
+document.querySelector('.more').addEventListener('click',function(){
+document.querySelectorAll('details').forEach(e=>e.remove());
+(this).remove();
 
 xmlDoc.querySelectorAll('item').forEach(e=>{
-document.querySelector('main').innerHTML += '<p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('title').innerHTML + '</a></p>';
+document.querySelector('main').innerHTML += '<details><summary>' + e.querySelector('title').innerHTML + '</summary><p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('description').innerHTML + '</a></p></details>';
 });
 
 });
