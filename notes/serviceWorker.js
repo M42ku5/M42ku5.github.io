d7bdb7c6224b16c0bef2b9b2bb1adeecd8b7440a
Markus <51908256+M42ku5@users.noreply.github.com>
2021-11-01 00:01:56 +0100

    .

---
+++ notes/serviceWorker.js
@@ -0,0 +1,25 @@
+const myPwaApp = "v1";
+const assets = [
+  "/notes/",
+  "/notes/index.html",
+  "/notes/scripts.min.js",
+  "/notes/style.min.js",
+  "/notes/images/icon-192.png",
+  "/notes/images/icon-512.png"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(myPwaApp).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
