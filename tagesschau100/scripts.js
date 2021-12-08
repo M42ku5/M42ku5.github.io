081314f5c943a05bb158745c28c01d5971bbaf15
Markus <51908256+M42ku5@users.noreply.github.com>
2021-12-08 22:33:11 +0100

    Add files via upload

--- tagesschau100/scripts.js
+++ tagesschau100/scripts.js
@@ -1,42 +1,45 @@
 fetch("https://www.tagesschau.de/export/podcast/hi/tagesschau-in-100-sekunden/")
   .then((res) => res.text())
   .then((data) => {
-    let parser = new DOMParser();
-    let xmlDoc = parser.parseFromString(data, "text/xml");
-    let audioUrl = xmlDoc
-      .getElementsByTagName("enclosure")[0]
-      .getAttribute("url");
-    // console.log(audioUrl);
-    let pubDate = xmlDoc.querySelector("item pubDate").innerHTML;
-    // console.log(pubDate);
-    let dateObj = new Date(pubDate);
-    
-    let currentHours = dateObj.getHours();
-    let currentMinutes = dateObj.getMinutes();
-    let formatHours =  currentHours > 10 ? currentHours : '0' + currentHours;
-    let formatMinutes = currentMinutes > 10 ? currentMinutes : '0' + currentMinutes;
-    let pubTime = formatHours + ":" + formatMinutes;
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(data, 'text/xml');
+    const audioUrl = xmlDoc.querySelector('enclosure').getAttribute('url');
+    const pubDate = xmlDoc.querySelector("item pubDate").innerHTML;
+    const dateObj = new Date(pubDate);
 
-    // console.log(pubTime);
-    document.getElementsByTagName("section")[0].innerHTML =
-      '<p>Beitrag von: ' +
-      pubTime +
-      ' Uhr</p><audio controls><source src="' +
-      audioUrl +
-      '" type="audio/mpeg">Your browser does not support the audio tag.</audio><p>Geschwindigkeit:<br><button class="speed-btn1">1x</button> <button class="speed-btn2">2x</button></p><p><a href="' +
-      audioUrl +
-      '">Quelle</a></p>';
-    document.getElementsByTagName("audio")[0].playbackRate = "2";
-    document
-      .getElementsByClassName("speed-btn1")[0]
-      .addEventListener(
-        "click",
-        () => (document.getElementsByTagName("audio")[0].playbackRate = "1")
-      );
-    document
-      .getElementsByClassName("speed-btn2")[0]
-      .addEventListener(
-        "click",
-        () => (document.getElementsByTagName("audio")[0].playbackRate = "2")
-      );
+    const currentHours = dateObj.getHours();
+    const currentMinutes = dateObj.getMinutes();
+    const formatHours =  currentHours > 10 ? currentHours : '0' + currentHours;
+    const formatMinutes = currentMinutes > 10 ? currentMinutes : '0' + currentMinutes;
+    const pubTime = formatHours + ":" + formatMinutes;
+
+    document.querySelector('section').innerHTML =
+      `<p>Beitrag von: ${pubTime} Uhr</p>
+      <audio controls>
+      <source src="${audioUrl}" type="audio/mpeg">Your browser does not support the audio tag.
+      </audio>
+      <p>Geschwindigkeit:<br>
+      <button class="speed-btn1">1x</button> <button class="speed-btn2">2x</button> <button class="speed-btn3">2.5x</button>
+      </p>
+      <p><label>
+      <input type="checkbox" checked> Intro überspringen</label></p>
+      <p><a href="${audioUrl}">Quelle</a></p>`;
+
+      function skipIntro(x) {
+        if(!document.querySelector('audio').paused){
+          document.querySelector('audio').pause();
+        }
+        if(x.checked){
+          document.querySelector('audio').currentTime = 10;
+        } else {
+          document.querySelector('audio').currentTime = 0;
+        }
+      }
+
+    document.querySelector('audio').playbackRate = 2;
+    document.querySelector('.speed-btn1').addEventListener('click',() => document.querySelector('audio').playbackRate = 1);
+    document.querySelector('.speed-btn2').addEventListener('click',() => document.querySelector('audio').playbackRate = 2);
+    document.querySelector('.speed-btn3').addEventListener('click',() => document.querySelector('audio').playbackRate = 2.5);
+    document.querySelector('input[type=checkbox]').addEventListener('change',function(){skipIntro(this)});
+    document.querySelector('audio').currentTime = 10;
   });
