58210cda8c946cda6e41ded2cc0d12fd2374f6eb
Markus <51908256+M42ku5@users.noreply.github.com>
2022-01-01 13:22:30 +0100

    Add files via upload

--- tagesschau100/scripts.js
+++ tagesschau100/scripts.js
@@ -1,55 +1,48 @@
 fetch("https://www.tagesschau.de/export/podcast/hi/tagesschau-in-100-sekunden/")
-  .then((res) => res.text())
-  .then((data) => {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(data, 'text/xml');
-    const audioUrl = xmlDoc.querySelector('enclosure').getAttribute('url');
-    const pubDate = xmlDoc.querySelector("item pubDate").innerHTML;
-    const dateObj = new Date(pubDate);
-
-    const currentHours = dateObj.getHours();
-    const currentMinutes = dateObj.getMinutes();
-    const formatHours =  currentHours > 10 ? currentHours : '0' + currentHours;
-    const formatMinutes = currentMinutes > 10 ? currentMinutes : '0' + currentMinutes;
-    const pubTime = formatHours + ":" + formatMinutes;
-
-    document.querySelector('section').innerHTML =
-      `<p>Beitrag von: ${pubTime} Uhr</p>
-      <audio controls>
-      <source src="${audioUrl}" type="audio/mpeg">Your browser does not support the audio tag.
-      </audio>
-      <p>Geschwindigkeit:<br>
-      <div class="speed-wrapper">
-      <input type="range" class="speed" value="2.2" min="1" max="3.5" step="0.1"> <span class="speed-status"></span>
-      </div>
-      </p>
-      <p><label>
-      <input type="checkbox" checked> Intro überspringen</label></p>
-      <p><a href="${audioUrl}">Quelle</a></p>`;
-
-      function skipIntro(x) {
-        if(!document.querySelector('audio').paused){
-          document.querySelector('audio').pause();
-        }
-        if(x.checked){
-          document.querySelector('audio').currentTime = 13;
-        } else {
-          document.querySelector('audio').currentTime = 0;
-        }
-      }
-
-      function updateSpeedStatus(){
-        document.querySelector('.speed-status').innerHTML = document.querySelector('.speed').value;
-      }
-
-    document.querySelector('audio').playbackRate = document.querySelector('.speed').value;
-    document.querySelector('.speed').addEventListener('change',() => {
-    document.querySelector('audio').playbackRate = document.querySelector('.speed').value;
-    updateSpeedStatus();
-    });
-    document.querySelector('input[type=checkbox]').addEventListener('change',function(){skipIntro(this)});
-    document.querySelector('audio').currentTime = 13;
-
-    updateSpeedStatus();
-
-  });
+.then((res)=>res.text())
+.then((data)=>{
+const parser=new DOMParser();
+const xmlDoc=parser.parseFromString(data, 'text/xml');
+const audioUrl=xmlDoc.querySelector('enclosure').getAttribute('url');
+const pubDate=xmlDoc.querySelector("item pubDate").innerHTML;
+const dateObj=new Date(pubDate);
+const currentHours=dateObj.getHours();
+const currentMinutes=dateObj.getMinutes();
+const formatHours=currentHours >= 10 ? currentHours : '0' + currentHours;
+const formatMinutes=currentMinutes >= 10 ? currentMinutes : '0' + currentMinutes;
+const pubTime=formatHours + ":" + formatMinutes;
+document.querySelector('section').innerHTML=
+`<p>Beitrag von: ${pubTime} Uhr</p>
+<audio controls>
+<source src="${audioUrl}" type="audio/mpeg">Your browser does not support the audio tag.
+</audio>
+<p>Geschwindigkeit:<br>
+<div class="speed-wrapper">
+<input type="range" class="speed" value="2.2" min="1" max="3.5" step="0.1"> <span class="speed-status"></span>
+</div>
+</p>
+<p><label>
+<input type="checkbox" checked> Intro überspringen</label></p>
+<p><a href="${audioUrl}">Quelle</a></p>`;
+function skipIntro(x){
+if(!document.querySelector('audio').paused){
+document.querySelector('audio').pause();
+}
+if(x.checked){
+document.querySelector('audio').currentTime=13;
+}else{
+document.querySelector('audio').currentTime=0;
+}
+}
+function updateSpeedStatus(){
+document.querySelector('.speed-status').innerHTML=document.querySelector('.speed').value;
+}
+document.querySelector('audio').playbackRate=document.querySelector('.speed').value;
+document.querySelector('.speed').addEventListener('change',()=>{
+document.querySelector('audio').playbackRate=document.querySelector('.speed').value;
+updateSpeedStatus();
+});
+document.querySelector('input[type=checkbox]').addEventListener('change',function(){skipIntro(this)});
+document.querySelector('audio').currentTime=13;
+updateSpeedStatus();
+});
