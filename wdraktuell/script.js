895e27ffbc91179da9c5b132530c3e9bfb534870
Markus <51908256+M42ku5@users.noreply.github.com>
2022-01-16 17:37:38 +0100

    Add files via upload

--- wdraktuell/script.js
+++ wdraktuell/script.js
@@ -13,42 +13,33 @@ let output = document.querySelector('.output');
 if(this.readyState == 4 && this.status == 200){
 let xmlDoc = this.responseXML;
 let txt = '';
-let description = xmlDoc.querySelectorAll('description');
-let url = xmlDoc.querySelectorAll('enclosure[url]');
-for(let i = 0; i < 1; i++){
-txt = txt + '<li> <a href="' + url[i].getAttribute('url') + '" target="_blank">' + description[i+1].innerHTML + '</a> <span class="note">(normale Geschw.)</span><div class="controls"><button onclick="playAudio(this)"><div class="font-size-14 padding-top-1">&#9654;</div><div class="font-size-9 letter-spacing-0">&#10073;&#10073;</div></button><audio><source src="'+ url[i].getAttribute('url') +'" type="audio/mpeg"> Your browser does not support the audio element.</audio><span class="time"></span> <span class="note">(2-fache Geschw.)</span></div></li>';
-}
-output.innerHTML = '<ul>' + txt + '</ul>';
-const audioHook = document.querySelectorAll('audio');
-function setPlaySpeed(){
-for(let i = 0; i < audioHook.length; i++){
-audioHook[i].defaultPlaybackRate = 2.0;
-audioHook[i].load();
-}
-}
-setPlaySpeed();
+let description = xmlDoc.querySelector('description');
+let url = xmlDoc.querySelector('enclosure[url]');
 
-const playTimeHook = document.querySelectorAll('.time');
+txt = `${txt}<li> <a href="${url.getAttribute('url')}" target="_blank">${description.innerHTML}</a> <span class="note">(normale Geschw.)</span><div class="controls"><button onclick="playAudio(this)"><div class="font-size-14 padding-top-1">&#9654;</div><div class="font-size-9 letter-spacing-0">&#10073;&#10073;</div></button><audio><source src="${url.getAttribute('url')}" type="audio/mpeg"> Your browser does not support the audio element.</audio><span class="time"></span><button onclick="setPlaySpeed(1)">1x</button><button onclick="setPlaySpeed(1.5)">1.5x</button><button onclick="setPlaySpeed(2)">2x</button></div></li>`;
 
+output.innerHTML = `<ul>${txt}</ul>`;
+const audioHook = document.querySelector('audio');
+function setPlaySpeed(x){
+audioHook.defaultPlaybackRate=x;
+audioHook.load();
+}
+const playTimeHook = document.querySelector('.time');
 function showPlayTime(){
-for(let i = 0; i < audioHook.length; i++){
-audioHook[i].addEventListener(
-"timeupdate",() =>{
-let cTime = audioHook[i].currentTime;
+audioHook.addEventListener(
+"timeupdate",()=>{
+let cTime = audioHook.currentTime;
 let zero = '';
 if(Math.floor(cTime) % 60 < 10){
 zero = 0;
-};
-playTimeHook[i].innerHTML =( Math.floor(Math.floor(cTime) / 60) ) + ':' + zero +(Math.floor(cTime) % 60);
-
+}
+playTimeHook.innerHTML=(Math.floor(Math.floor(cTime) / 60)) + ':' + zero +(Math.floor(cTime) % 60);
 }
 );
 }
-}
 showPlayTime();
-
 }else if(this.status > 200){
-output.innerHTML = 'Status: ' + this.status;
+output.innerHTML = `Status: ${this.status}`;
 }
 };
 let corsProxy = 'https://corsproxyms.herokuapp.com/';
