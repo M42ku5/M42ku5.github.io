4af2496dc948587102d6c6870c4f39ee08492631
Markus <51908256+M42ku5@users.noreply.github.com>
2022-01-16 18:30:21 +0100

    Add files via upload

--- wdraktuell/script.js
+++ wdraktuell/script.js
@@ -5,6 +5,9 @@ el.nextSibling.pause();
 el.nextSibling.play();
 }
 }
+function setPlaySpeed(x){
+document.querySelector('audio').playbackRate=x;
+}
 window.addEventListener('load',() => loadSource('https://www1.wdr.de/mediathek/audio/wdr-aktuell-news/wdr-aktuell-152.podcast'));
 let loadSource =(NewsUrl) =>{
 let xhr = new XMLHttpRequest();
@@ -12,18 +15,10 @@ xhr.onreadystatechange = function(){
 let output = document.querySelector('.output');
 if(this.readyState == 4 && this.status == 200){
 let xmlDoc = this.responseXML;
-let txt = '';
-let description = xmlDoc.querySelector('description');
+let description = xmlDoc.querySelector('item description');
 let url = xmlDoc.querySelector('enclosure[url]');
-
-txt = `${txt}<li> <a href="${url.getAttribute('url')}" target="_blank">${description.innerHTML}</a> <span class="note">(normale Geschw.)</span><div class="controls"><button onclick="playAudio(this)"><div class="font-size-14 padding-top-1">&#9654;</div><div class="font-size-9 letter-spacing-0">&#10073;&#10073;</div></button><audio><source src="${url.getAttribute('url')}" type="audio/mpeg"> Your browser does not support the audio element.</audio><span class="time"></span><button onclick="setPlaySpeed(1)">1x</button><button onclick="setPlaySpeed(1.5)">1.5x</button><button onclick="setPlaySpeed(2)">2x</button></div></li>`;
-
-output.innerHTML = `<ul>${txt}</ul>`;
+output.innerHTML = `<ul><li><a href="${url.getAttribute('url')}" target="_blank">${description.innerHTML}</a> <span class="note">(normale Geschw.)</span><div class="controls"><button onclick="playAudio(this)"><div class="font-size-14 padding-top-1">&#9654;</div><div class="font-size-9 letter-spacing-0">&#10073;&#10073;</div></button><audio><source src="${url.getAttribute('url')}" type="audio/mpeg"> Your browser does not support the audio element.</audio><span class="time"></span><button onclick="setPlaySpeed(1)">1x</button><button onclick="setPlaySpeed(1.5)">1.5x</button><button onclick="setPlaySpeed(2)">2x</button></div></li></ul>`;
 const audioHook = document.querySelector('audio');
-function setPlaySpeed(x){
-audioHook.defaultPlaybackRate=x;
-audioHook.load();
-}
 const playTimeHook = document.querySelector('.time');
 function showPlayTime(){
 audioHook.addEventListener(
