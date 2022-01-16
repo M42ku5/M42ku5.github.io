4405dd72df96b47942343d4fdbd409ff289773e6
Markus <51908256+M42ku5@users.noreply.github.com>
2022-01-16 18:49:58 +0100

    Add files via upload

--- wdraktuell/script.js
+++ wdraktuell/script.js
@@ -1,28 +1,29 @@
-function playAudio(el){
-if(!el.nextSibling.paused){
-el.nextSibling.pause();
+function playAudio(){
+const el=document.querySelector('audio');
+if(!el.paused){
+el.pause();
 }else{
-el.nextSibling.play();
+el.play();
 }
 }
 function setPlaySpeed(x){
 document.querySelector('audio').playbackRate=x;
 }
-window.addEventListener('load',() => loadSource('https://www1.wdr.de/mediathek/audio/wdr-aktuell-news/wdr-aktuell-152.podcast'));
-let loadSource =(NewsUrl) =>{
+window.addEventListener('load',()=>loadSource('https://www1.wdr.de/mediathek/audio/wdr-aktuell-news/wdr-aktuell-152.podcast'));
+let loadSource=(NewsUrl)=>{
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
-let output = document.querySelector('.output');
+let output=document.querySelector('.output');
 if(this.readyState == 4 && this.status == 200){
-let xmlDoc = this.responseXML;
-let description = xmlDoc.querySelector('item description');
-let url = xmlDoc.querySelector('enclosure[url]');
-output.innerHTML = `
+let xmlDoc=this.responseXML;
+let description=xmlDoc.querySelector('item description');
+let url=xmlDoc.querySelector('enclosure[url]');
+output.innerHTML=`
 <ul>
 <li>
 <a href="${url.getAttribute('url')}" target="_blank">${description.innerHTML}</a>
 <div class="controls">
-<button onclick="playAudio(this)"><div class="font-size-14 padding-top-1">&#9654;</div>
+<button onclick="playAudio()"><div class="font-size-14 padding-top-1">&#9654;</div>
 <div class="font-size-9 letter-spacing-0">&#10073;&#10073;</div>
 </button>
 <audio>
@@ -47,7 +48,7 @@ audioHook.addEventListener(
 let cTime = audioHook.currentTime;
 let zero = '';
 if(Math.floor(cTime) % 60 < 10){
-zero = 0;
+zero=0;
 }
 playTimeHook.innerHTML=`{Math.floor(Math.floor(cTime) / 60)}:${zero}${(Math.floor(cTime) % 60)}`;
 }
@@ -55,10 +56,10 @@ playTimeHook.innerHTML=`{Math.floor(Math.floor(cTime) / 60)}:${zero}${(Math.floo
 }
 showPlayTime();
 }else if(this.status > 200){
-output.innerHTML = `Status: ${this.status}`;
+output.innerHTML=`Status: ${this.status}`;
 }
 };
-let corsProxy = 'https://corsproxyms.herokuapp.com/';
+let corsProxy='https://corsproxyms.herokuapp.com/';
 xhr.open('GET', corsProxy + NewsUrl, true);
 xhr.send();
 }
