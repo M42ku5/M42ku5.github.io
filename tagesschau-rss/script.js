b248e37a28387d991bd9fdf83e28ff55ab9dd50d
Markus <51908256+M42ku5@users.noreply.github.com>
2022-01-24 00:00:32 +0100

    Update script.js

--- tagesschau-rss/script.js
+++ tagesschau-rss/script.js
@@ -1,9 +1,19 @@
+if(localStorage.getItem('xmlDoc')){
+const raw = localStorage.getItem('xmlDoc');
+const xmlDoc = parser.parseFromString(raw,"text/xml");
+xmlDoc.querySelectorAll('item').forEach(e=>{
+document.querySelector('main').innerHTML += '<details><summary>' + e.querySelector('title').innerHTML + '</summary><p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('description').innerHTML + '</a></p></details>';
+});
+}
+
 fetch('https://www.tagesschau.de/xml/rss2_https/')
 .then(x => x.text())
 .then(y => {
 let parser = new DOMParser();
 let xmlDoc = parser.parseFromString(y,"text/xml");
 let txt = '';
+document.querySelector('main').innerHTML='';
+localStorage.setItem('xmlDoc', y);  
 [...xmlDoc.querySelectorAll('item')].slice(0, 10).forEach(e=>{
 txt += '<details><summary>' + e.querySelector('title').innerHTML + '</summary><p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('description').innerHTML + '</a></p></details>';
 });
