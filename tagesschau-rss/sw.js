1ba71c55757353e191d3284d00f83a38ae7017af
Markus <51908256+M42ku5@users.noreply.github.com>
2022-02-28 22:07:37 +0100

    Add files via upload

---
+++ tagesschau-rss/sw.js
@@ -0,0 +1,26 @@
+const myPwaApp = "v1";
+const assets = [
+  "/tagesschau-rss/",
+  "/tagesschau-rss/index.html",
+  "/tagesschau-rss/script.js",
+  "/tagesschau-rss/images/icon-192.png",
+  "/tagesschau-rss/images/icon-192-maskable.png",
+  "/tagesschau-rss/images/icon-512.png"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(myPwaApp).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request)
+    .then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
