97695fc99c18c425b1d4897435c00ad69f49983f
Markus <51908256+M42ku5@users.noreply.github.com>
2022-04-27 00:12:14 +0200

    Update script.js

--- tagesschau-rss/script.js
+++ tagesschau-rss/script.js
@@ -1,43 +1,30 @@
 const sourceUrl = "https://www.tagesschau.de/xml/rss2_https/";
 
-function showData(y) {
+function getTime(input) {
+  const date = new Date(input);
+  return date.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+    })
+}
+
+function showData(data) {
     const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(y, "text/xml");
+    const xmlDoc = parser.parseFromString(data, "text/xml");
     let txt = '';
     document.querySelector('main').innerHTML = '';
-    localStorage.setItem('xmlDoc', y);
-    [...xmlDoc.querySelectorAll('item')].slice(0, 10).forEach(e => {
-        txt += '<details><summary>' + e.querySelector('title').innerHTML + '</summary><p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('description').innerHTML + '</a></p></details>';
+    
+    [...xmlDoc.querySelectorAll('item')].forEach(e => {
+        txt += `<details><summary><span class="time">${getTime(e.querySelector('pubDate').innerHTML)}:</span> ${e.querySelector('title').innerHTML}</summary><p><a href="${e.querySelector('link').innerHTML}" target="_blank" rel="noreferrer">${e.querySelector('description').innerHTML}</a></p></details>`;
     });
-    document.querySelector('main').innerHTML = txt + '<p class="more">mehr anzeigen</p>';
-    document.querySelector('.more').addEventListener('click', function () {
-        document.querySelectorAll('details').forEach(e => e.remove());
-        this.remove();
 
-        xmlDoc.querySelectorAll('item').forEach(e => {
-            document.querySelector('main').innerHTML += '<details><summary>' + e.querySelector('title').innerHTML + '</summary><p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('description').innerHTML + '</a></p></details>';
-        });
-
-    });
 }
 
 function getExternalData(url) {
 fetch(url)
-    .then(x => x.text())
-    .then(y => {
-        showData(y)
-    });
-}
-
-if (localStorage.getItem('xmlDoc')) {
-    const raw = localStorage.getItem('xmlDoc');
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(raw, "text/xml");
-    [...xmlDoc.querySelectorAll('item')].slice(0, 10).forEach(e => {
-        document.querySelector('main').innerHTML += '<details><summary>' + e.querySelector('title').innerHTML + '</summary><p><a href="' + e.querySelector('link').innerHTML + '" target="_blank" rel="noreferrer">' + e.querySelector('description').innerHTML + '</a></p></details>';
-    });
-} else {
-    getExternalData(sourceUrl)
+  .then(response => response.text())
+  .then(data => showData(data));
 }
 
 document.querySelector(".reload").addEventListener("click", function() {
