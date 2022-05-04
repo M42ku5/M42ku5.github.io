a565a7fee5e0670bb388b5a6fac6269db926ef96
Markus <51908256+M42ku5@users.noreply.github.com>
2022-05-05 00:27:20 +0200

    Add files via upload

--- tagesschau-rss/script.js
+++ tagesschau-rss/script.js
@@ -2,7 +2,7 @@ const sourceUrl = "https://www.tagesschau.de/xml/rss2_https/"
 function getTime(input) { const date = new Date(input); return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }) }
 const showTime = (condition, result) => {
   if (condition === 0){
-    return result
+    return `<span class="time">${result}</span> `
   } else {
     return ""
   }
@@ -12,7 +12,7 @@ function showData(data) {
   const xmlDoc = parser.parseFromString(data, "text/xml")
   let txt = ""
   xmlDoc.querySelectorAll('item').forEach((e,i) => {
-    txt += `<details><summary onclick="if(!this.parentElement.querySelector('p')){const para=document.createElement('p');this.parentElement.appendChild(para);const el=document.createElement('a');el.setAttribute('href','${e.querySelector('link').innerHTML}');el.setAttribute('target','_blank');el.setAttribute('rel','noreferrer');el.innerHTML='${e.querySelector('title').innerHTML.replace(/\"/g, "&quot;")}: ${e.querySelector('description').innerHTML.replace(/\"/g, "&quot;")}';para.appendChild(el)}"><span class="time">${showTime(i,getTime(e.querySelector('pubDate').innerHTML))}</span> ${e.querySelector('title').innerHTML}</summary></details>`
+    txt += `<details><summary onclick="if(!this.parentElement.querySelector('p')){const para=document.createElement('p');this.parentElement.appendChild(para);const el=document.createElement('a');el.setAttribute('href','${e.querySelector('link').innerHTML}');el.setAttribute('target','_blank');el.setAttribute('rel','noreferrer');el.innerHTML='${e.querySelector('title').innerHTML.replace(/\"/g, "&quot;")}: ${e.querySelector('description').innerHTML.replace(/\"/g, "&quot;")}';para.appendChild(el)}">${showTime(i,getTime(e.querySelector('pubDate').innerHTML))}${e.querySelector('title').innerHTML}</summary></details>`
   })
   document.querySelector('main').innerHTML = ""
   document.querySelector('main').innerHTML = txt
