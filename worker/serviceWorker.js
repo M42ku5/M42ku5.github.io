f264cde2f04c9aec20d266a76063d9d357e3d4b0
Markus <51908256+M42ku5@users.noreply.github.com>
2023-08-06 01:10:49 +0200

    Add files via upload

--- worker/serviceWorker.js
+++ worker/serviceWorker.js
@@ -1,4 +1,4 @@
 self.addEventListener('message', function(event) {
   console.log('Service Worker received a message', event.data);
-  self.clients.matchAll().then(all => all.forEach(client => client.postMessage(event.data)));
+  this.setInterval(() => {self.clients.matchAll().then(all => all.forEach(client => client.postMessage(event.data)));}, 1000);
 });
