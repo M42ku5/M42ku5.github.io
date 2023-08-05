ca236dcbff91c3aef0069fcb15f014b15bad983a
Markus <51908256+M42ku5@users.noreply.github.com>
2023-08-06 01:20:10 +0200

    Add files via upload

--- worker/serviceWorker.js
+++ worker/serviceWorker.js
@@ -1,4 +1,16 @@
+let intervalID;
+
 self.addEventListener('message', function(event) {
   console.log('Service Worker received a message', event.data);
-  this.setInterval(() => {self.clients.matchAll().then(all => all.forEach(client => client.postMessage(event.data)));}, 1000);
+  
+  if (event.data === 'confirmation') {
+    // Clear the interval if a confirmation message is received
+    clearInterval(intervalID);
+    console.log('Interval cleared');
+    return;
+  }
+  
+  intervalID = setInterval(() => {
+    self.clients.matchAll().then(all => all.forEach(client => client.postMessage(event.data)));
+  }, 1000);
 });
