d0a6e1592b578e493bddb9d1b51fd1b1b201bd11
Markus <51908256+M42ku5@users.noreply.github.com>
2023-08-06 21:16:53 +0200

    Add files via upload

--- worker/serviceWorker.js
+++ worker/serviceWorker.js
@@ -1,19 +1,48 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
-});
+// Use a library or helper for IndexedDB operations
+const dbVersion = 1;
+const dbName = 'ImageDB';
 
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated');
-});
+function openDB() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open(dbName, dbVersion);
+
+        request.onerror = event => {
+            console.error("Error opening DB", event);
+            reject("Error");
+        };
+
+        request.onsuccess = event => {
+            resolve(event.target.result);
+        };
+
+        request.onupgradeneeded = event => {
+            const db = event.target.result;
+            if (!db.objectStoreNames.contains('images')) {
+                db.createObjectStore('images');
+            }
+        };
+    });
+}
 
 self.addEventListener('message', async (event) => {
     if (event.data && event.data.image) {
-        console.log('Received image in Service Worker. Storing in Cache...');
-        const base64data = "data:image/jpeg;base64," + event.data.image;
-        const cache = await caches.open('image-cache');
-        const response = new Response(base64data);
-        cache.put('uploaded-image', response);
-        console.log('Image stored in Cache.');
+        console.log('Received image in Service Worker. Storing in IndexedDB...');
+        
+        // Use the provided MIME type or default to JPEG if not available
+        const base64data = "data:" + (event.data.type || "image/jpeg") + ";base64," + event.data.image;
+
+        const db = await openDB();
+        const transaction = db.transaction(['images'], 'readwrite');
+        const store = transaction.objectStore('images');
+        store.put(base64data, 'uploaded-image');
+
+        transaction.oncomplete = function(event) {
+            console.log('Image stored in IndexedDB.');
+        };
+
+        transaction.onerror = function(event) {
+            console.error('Error storing image:', event);
+        };
     } else {
         console.log('No image data received in Service Worker');
     }
