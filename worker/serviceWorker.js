1a183336c1b60aafd1071480738fbadc14c2f11a
Markus <51908256+M42ku5@users.noreply.github.com>
2023-08-06 20:56:04 +0200

    Add files via upload

--- worker/serviceWorker.js
+++ worker/serviceWorker.js
@@ -9,13 +9,9 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('message', async (event) => {
     if (event.data && event.data.image) {
         console.log('Received image in Service Worker. Storing in Cache...');
+        const base64data = "data:image/jpeg;base64," + event.data.image;
         const cache = await caches.open('image-cache');
-        const response = new Response(event.data.image, {
-            headers: {
-                'Content-Type': 'image/jpeg',
-                'Cache-Control': 'max-age=3600',
-            },
-        });
+        const response = new Response(base64data);
         cache.put('uploaded-image', response);
         console.log('Image stored in Cache.');
     } else {
